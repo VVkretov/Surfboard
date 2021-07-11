018d49193ac1ff98e6233ea1d2776ab5e7f15cf9
Vadim <v.v.kretov@mail.ru>
2021-07-12 03:16:22 +0700

    исправил ошибки в js

--- JS/custom.js
+++ JS/custom.js
@@ -210,82 +210,82 @@ $(".team-list__top").click(e => {
  //////////////////////////////////////////////////////////player///////////////////////////////////////////////////////// 
 
 
- let player;
-//  const playerContainer = $(".player");
+let player;
+const playerContainer = $(".player");
  
-//  let eventsInit = () => {
-//      $(".player__start").click(e => {
-//          e.preventDefault();
+let eventsInit = () => {
+    $(".player__start").click(e => {
+        e.preventDefault();
+
+        if(playerContainer.hasClass("paused")) {
+            playerContainer.removeClass("paused");
+            player.pauseVideo();
+        } else {
+            playerContainer.addClass("paused");
+            player.playVideo();
+        }
  
-//          if(playerContainer.hasClass("paused")) {
-//              playerContainer.removeClass("paused");
-//              player.pauseVideo();
-//          } else {
-//              playerContainer.addClass("paused");
-//              player.playVideo();
-//          }
+    });
+};
  
-//      });
-//  };
+const formatTime = timeSec => {
+    const roundTime = Math.round(timeSec);
  
-//  const formatTime = timeSec => {
-//      const roundTime = Math.round(timeSec);
+    const minutes = addZero(Math.floor(roundTime / 60));
+    const seconds = addZero(roundTime - minutes * 60);
  
-//      const minutes = addZero(Math.floor(roundTime / 60));
-//      const seconds = addZero(roundTime - minutes * 60);
+    function addZero(num) {
+        return num < 10 ? `0${num}` : num;
+    };  
  
-//      function addZero(num) {
-//          return num < 10 ? `0${num}` : num;
-//      }; 
- 
-//      return `${minutes} : ${seconds}`;
-//  }
+    return `${minutes} : ${seconds}`;
+};
  
  
-//  const onPlayerReady = () => {
-//      let interval;
-//      const durationSec = player.getDuration();
+const onPlayerReady = () => { 
+    let interval;
+    const durationSec = player.getDuration();
  
-//      $(".player__duration-estimate").text(formatTime(durationSec));
+    $(".player__duration-estimate").text(formatTime(durationSec));
  
-//      if(typeof interval !== "undefined") {
-//          clearInterval(interval);
-//      };
+    if(typeof interval !== "undefined") {
+        clearInterval(interval);
+    };
      
-//      interval = setInterval(() => {
-//          const completedSec = player.getСurrentTime();
+    interval = setInterval(() => {
+        const completedSec = player.getСurrentTime();
+        player.playVideo();
+        // const completedPercent = (completedSec / durationSec) * 100;
 
-//          const completedPercent = (completedSec / durationSec) * 100;
-
-//          $(".player__playback-button").css({
-//              left: `${completedPercent}%`
-//          });
+//         $(".player__playback-button").css({
+//             left: `${completedPercent}%`
+//         });
  
-//          $(".player__duration-completed").text(formatTime(completedSec))
-//      }, 1000);
-//  };
+        $(".player__duration-completed").text(formatTime(completedSec));
+    }, 1000);
+};
  
-       function onYouTubeIframeAPIReady() {
-         player = new YT.Player('yt-player', {
-           height: '390',
-           width: '660',
-           videoId: 'FmzHq3U0UFE',
-           events: {
-             onReady: onPlayerReady,
-             // 'onStateChange': onPlayerStateChange
-           },
-           playerVars: {
-               controls: 0,
-               disablekd: 0,
-               showinfo: 0,
-               rel: 0,
-               autoplay: 0,
-               modestbranding: 0
-           }
-         });
-       };
- 
-
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('yt-player', {
+    height: '390',
+    width: '660',
+    videoId: 'FmzHq3U0UFE',
+    events: {
+    onReady: onPlayerReady,
+     // 'onStateChange': onPlayerStateChange
+    },
+    playerVars: {
+        controls: 0,
+        disablekd: 0,
+        showinfo: 0,
+        rel: 0,
+        autoplay: 0,
+        modestbranding: 0,
+    }
+    });
+};
+    
+eventsInit();
 
 //////////////////////////////////////////////////////////forma///////////////////////////////////////////////////////// 
 
